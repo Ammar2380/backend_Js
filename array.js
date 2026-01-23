114694032971c05850642fdd67f7959f368102cb
Ammar Jamali <ammar211132@gmail.com>
2026-01-23 17:17:01 +0500

    arr

--- array.js
+++ array.js
@@ -4,3 +4,14 @@ for(let el of a){
     i+=el
 }
 i/a.length
+
+var arr = [1,2,3,4,5,6]
+arr.forEach(function(val){
+    console.log(val + " hello")
+})
+
+var ans = arr.map(function(val){ // take space of array you have maped and then it will add what you have returned
+    return 13;
+})
+
+console.log(ans)
